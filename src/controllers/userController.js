677ad58cc21daf570c29361be8bf86bf6c80ad90
Saquib Akhtar <saquibmohammad38@gmail.com>
2022-11-07 23:26:01 +0530

    add try & catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,84 +1,110 @@
-const userModel =require("../models/userModel")
-const jwt = require("jsonwebtoken")
-
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 //**********************************************createApi */
-const createUser = async(req,res)=>{
+const createUser = async (req, res) => {
+  try {
+    let requestBody = req.body;
 
-let requestBody = req.body
-
-const userCreation = await userModel.create(requestBody)
-res.send({status:true,data:userCreation,msg:"user created successfuly "})
-}
+    const userCreation = await userModel.create(requestBody);
+    return res.status(201).send({
+      status: true,
+      data: userCreation,
+      msg: "user created successfuly ",
+    });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.msg });
+  }
+};
 
 //=====================================login==============================================================
 
+const loginUser = async (req, res) => {
+  try {
+    let email = req.body.emailId;
+    let pass = req.body.password;
 
-const loginUser = async(req,res)=>{
-
-let email =req.body.emailId
-let pass = req.body.password
-
-if(!email||!pass){
-    res.send({status:false,msg:"email id and password are mandatory"})
-
-}
-if(email && pass){
- const loginDetails=   await userModel.findOne({emailId:email,password:pass})
- if(loginDetails){
-
-    const token = jwt.sign({createUser:loginDetails._id},"key.secret")
-    res.send({status:true,token:token})
- } 
- else {
-    return res.send({ status: false, msg: "invalid credentials" })
-}  
-}
-
-
-}
+    if (!email || !pass) {
+      res.send({ status: false, msg: "email id and password are mandatory" });
+    }
+    if (email && pass) {
+      const loginDetails = await userModel.findOne({
+        emailId: email,
+        password: pass,
+      });
+      if (loginDetails) {
+        const token = jwt.sign({ createUser: loginDetails._id }, "key.secret");
+        return res.status(200).send({ status: true, token: token });
+      } else {
+        return res
+          .status(404)
+          .send({ status: false, msg: "invalid credentials" });
+      }
+    }
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.msg });
+  }
+};
 
 //**********************fetch Api********************************* */
-const fetchUserDetails = async function(req,res){
-
-
-    const Id = req.params.Id
-    const userDetails = await userModel.findById({_id:Id})
-      res.send({status:true,data:userDetails})
-
-   }
-
+const fetchUserDetails = async function (req, res) {
+  try {
+    const Id = req.params.Id;
+    const userDetails = await userModel.findById({ _id: Id });
+    return res.status(200).send({ status: true, data: userDetails });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.msg });
+  }
+};
 
 //******************updatrApi********** */
-   const updateData = async(req,res)=>{
+const updateData = async (req, res) => {
+  try {
+    const userId = req.params.userId;
 
-const userId=req.params.userId 
+    let user = await userModel.findById(userId);
+    if (!user) {
+      res.status(404).send({ status: false, msg: "userID not Valid" });
+    }
 
- let user = await  userModel.findById(userId)
-   if(!user){
-      res.send({status:false,msg:"userID not Valid"})
-   }
+    let userData = req.body;
+    let UpdateDetails = await userModel.findOneAndUpdate(
+      { _id: userId },
+      userData
+    );
+    res
+      .status(200)
+      .send({ status: true, data: UpdateDetails, msg: "updated Successfully" });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.msg });
+  }
+};
+//***************************deleteApi**************** */
 
-   let userData =req.body
-   let UpdateDetails = await userModel.findOneAndUpdate({_id:userId},userData)
-   res.send({status:true,data:UpdateDetails,msg:"updated Successfully"})
+const DeleteData = async (req, res) => {
+  try {
+    let id = req.params.id;
 
-   }
- //***************************deleteApi**************** */
+    if (!id) {
+      res.send({ status: false, msg: "invalid ID" });
+    }
 
-   const DeleteData =async(req,res)=>{
+    const deleteUser = await userModel.findByIdAndDelete({ _id: id });
+    res.status(200).send({
+      status: true,
+      msg: "userDeleted successfully",
+      data: deleteUser,
+      isDeleted: true,
+    });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.msg });
+  }
+};
 
-  let id =req.params.id
-
-if(!id){
-   res.send({status:false,msg:"invalid ID"})
-}
-  
-const deleteUser =await userModel.findByIdAndDelete({_id:id, })
-res.send({status:true,msg:"userDeleted successfully",data:deleteUser,isDeleted:true})
-
-   }
-
-
-
-module.exports={createUser,loginUser,fetchUserDetails,updateData,DeleteData}
+module.exports = {
+  createUser,
+  loginUser,
+  fetchUserDetails,
+  updateData,
+  DeleteData,
+};
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,30 +1,27 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const userAuthentication = async(req,res,next) => {
-    
-        const token = req.headers["x-auth-token"]
-      
-
-        if(!token){
-            return res.status(403).send({status:false,msg:'missing authentication token in request'})
-          
-        }  
-        
-      let decoded = await jwt.verify(token,"key.secret")
-      if(!decoded){
-         
-          return res.send({status:false,message:"Invalid authentication token in request"})
-           
+const userAuthentication = async (req, res, next) => {
+  try {
+    const token = req.headers["x-auth-token"];
+    if (!token) {
+      return res
+        .status(403)
+        .send({
+          status: false,
+          msg: "missing authentication token in request",
+        });
+    }
+    let decoded = await jwt.verify(token, "key.secret");
+    if (!decoded) {
+      return res.send({
+        status: false,
+        message: "Invalid authentication token in request",
+      });
+    }
+    req.Id = decoded.Id;
+    next();
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.msg });
   }
-       
-        req.Id = decoded.Id
-            next()
-        
-        
-        
-        
-    
-
-       
-}
-module.exports = userAuthentication
+};
+module.exports = userAuthentication;
